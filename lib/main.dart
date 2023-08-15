ca706736c24e75b98b470cb6d4c86eff7403e45c
vativeapps <contact@vativeapps.com>
2023-08-15 16:24:39 +0500

    4:25 | 15-Aug | HammasAhmed

--- lib/main.dart
+++ lib/main.dart
@@ -1,21 +1,17 @@
+//
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
-import 'package:podcastapp/screens/BottomNavigation.dart';
-import 'package:podcastapp/screens/LetYouIn.dart';
-import 'package:podcastapp/screens/Library_screen.dart';
-import 'package:podcastapp/screens/MainPageView.dart';
-import 'package:podcastapp/screens/On_boarding.dart';
-import 'package:podcastapp/screens/Otp_screen.dart';
-import 'package:podcastapp/screens/forget_password.dart';
-import 'package:podcastapp/screens/login.dart';
-import 'package:podcastapp/screens/new_password_screen.dart';
-import 'package:podcastapp/screens/signup_screen.dart';
 import 'package:podcastapp/screens/splash_screen.dart';
+// import 'package:zalada_app/service/fcm_token.dart';
+// import 'package:zalada_app/utiles/multi_language.dart';
+// import 'package:zalada_app/utiles/themeData.dart';
+// import 'MVC/views/splash_screen.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  // await FirebaseDB.init();
   runApp(const MyApp());
 }
 
@@ -23,20 +19,19 @@ class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return GetMaterialApp(
-      theme: ThemeData(
-        fontFamily: "PoppinsRegular",
-      ),
-      debugShowCheckedModeBanner: false,
-      // home: NewPassword(),
-      // home: LibraryScreen(),
-      home: splash_screen(),
-      // theme: ThemeData(
-      //   primarySwatch: Colors.purple,
-      // ),
-      // routes: {
-      //   signup.routeName: (context) => signup(),
-      // },
-    );
+    return ScreenUtilInit(
+        designSize: const Size(360, 690),
+        minTextAdapt: true,
+        splitScreenMode: true,
+        builder: (context, child) {
+          return GetMaterialApp(
+//             translations: Multi_Language(),
+//             locale: Locale('en', 'US'),
+//             themeMode: ThemeMode.light,
+            theme: ThemeData(fontFamily: "poppinsRegular"),
+            debugShowCheckedModeBanner: false,
+            home: splash_screen(),
+          );
+        });
   }
 }
